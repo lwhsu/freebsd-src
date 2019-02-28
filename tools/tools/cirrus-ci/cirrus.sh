6ecf1deea751bd5c42e2083380deac366b1c6979
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-02-28 20:08:32 +0000

    Build all universe targets

--- tools/tools/cirrus-ci/cirrus.sh
+++ tools/tools/cirrus-ci/cirrus.sh
@@ -2,11 +2,22 @@
 
 set -e
 
+MAKE_PARAMS=
 NCPU=$(sysctl -n hw.ncpu)
+NEED_XTOOLCHAIN_LIST="riscv64"
 
 cat >/etc/make.conf <<EOF
 NO_CLEAN=yes
 EOF
 
-make -j ${NCPU} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} buildworld
-make -j ${NCPU} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} buildkernel
+for i in ${NEED_XTOOLCHAIN_LIST}
+do
+	if [ ${i} = ${TARGET_ARCH} ]; then
+		pkg install -y ${i}-xtoolchain-gcc
+		MAKE_PARAMS="CROSS_TOOLCHAIN=${i}-gcc"
+		break
+	fi
+done
+
+make -j ${NCPU} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} ${MAKE_PARAMS} buildworld
+make -j ${NCPU} TARGET=${TARGET} TARGET_ARCH=${TARGET_ARCH} ${MAKE_PARAMS} buildkernel
